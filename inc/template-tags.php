86142be8c96cf32bf5631e9a0d02d09a54a0d61b
Clark Wimberly <clark@clarklab.net>
2012-05-23 23:30:58 -0500

    Light comment styling, redirect template

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -71,7 +71,7 @@ function _s_comment( $comment, $args, $depth ) {
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
 			<footer>
 				<div class="comment-author vcard">
-					<?php echo get_avatar( $comment, 40 ); ?>
+					<?php echo get_avatar( $comment, 60 ); ?>
 					<?php printf( __( '%s <span class="says">says:</span>', '_s' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
 				</div><!-- .comment-author .vcard -->
 				<?php if ( $comment->comment_approved == '0' ) : ?>
